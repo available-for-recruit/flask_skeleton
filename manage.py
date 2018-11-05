f219c5318b131e1559140e04737cc330e9889e39
available-for-recruit <44419348+available-for-recruit@users.noreply.github.com>
2018-11-05 09:49:57 -0800

    Disable coverage due to heroku issues

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 
 import unittest
 
-import coverage
+#import coverage
 
 from flask.cli import FlaskGroup
 
@@ -16,6 +16,7 @@ app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
 # code coverage
+"""
 COV = coverage.coverage(
     branch=True,
     include='project/*',
@@ -26,7 +27,7 @@ COV = coverage.coverage(
     ]
 )
 COV.start()
-
+"""
 
 @cli.command()
 def create_db():
